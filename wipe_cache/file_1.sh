31a6ba3b882f8ecc146f2a838d11dfc9473637ff
FlyCome <1453075013@qq.com>
2025-08-19 14:11:42 +0800

    创建 file_1.sh

---
+++ wipe_cache/file_1.sh
@@ -0,0 +1,48 @@
+#!/system/bin/sh
+#此脚本来自ClearBox模块，用于内部储存文件收集归类
+if [ ! "$(whoami)" = "root" ]; then
+    echo " » 请授予root权限！"
+    exit 1
+elif ! ClearBox -v >/dev/null; then
+    echo " » 模块加载异常，请排查反馈！"
+    exit 1
+fi
+######
+bin_dir=$(ClearBox -b)
+home_dir=$(ClearBox -h)
+work_dir=$(ClearBox -w)
+#exec 2>>/dev/null
+exec 2>>"$work_dir/运行日志.log"
+dir="/storage/emulated/0"
+######
+function FileAll()
+{
+ls "$work_dir/文件格式配置/" | while read NFile; do
+    if [ -d "$NFile" ]; then
+        rm -r "$NFile"
+        continue
+    else
+        Name=$(echo "$NFile" | cut -f1 -d ".")
+    fi
+    mkdir -p "$dir/Documents/$Name"
+    File_dir="$dir/Documents/$Name"
+    count_num=0
+    for Fn in $(cat "$work_dir/文件格式配置/$NFile"); do
+        for File in $("$bin_dir/busybox" find "$dir"/ -type f -name "*.$Fn"); do
+            if [ -f "$File" ]; then
+                mv "$File" "$File_dir"
+                count_num=$((count_num + 1))
+            fi
+        done
+    done
+    if [ "$count_num" -ge 1 ]; then
+        echo " » 已归类 $count_num 个 $Name"
+    fi
+done
+}
+######
+FileAll &
+wait
+echo " » 内部储存文件整理完成！"
+######
+exit 0
