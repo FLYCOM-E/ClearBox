10bc8f21ac15ff2ab5d8338d7c79edca8fa38675
FlyCome <1453075013@qq.com>
2025-09-02 23:11:01 +0800

    更新 action.sh

--- action.sh
+++ action.sh
@@ -12,8 +12,11 @@ bin_dir=$(ClearBox -b)
 home_dir=$(ClearBox -h)
 work_dir=$(ClearBox -w)
 source "$work_dir/settings.prop"
-#exec 2>>/dev/null
-exec 2>>"$work_dir/运行日志.log"
+if [ "$DebugPro" = 1 ]; then
+    exec 2>>"$work_dir/运行日志.log"
+else
+    exec 2>>/dev/null
+fi
 ######
 sh "$home_dir/all.sh" ClearAll &
 wait
