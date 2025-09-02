fcc10753ebb7b34fdaa9da7fa62813b3d0c59005
FlyCome <1453075013@qq.com>
2025-09-02 09:25:46 +0800

    更新 service.sh
    优化初始化逻辑

--- service.sh
+++ service.sh
@@ -59,18 +59,35 @@ if [ "$ClearIso_disk" = "" ]; then
     echo "ClearIso_disk=0" >> "$work_dir/settings.prop"
 fi
 ######
+function prosetting()
+{
+if [ -f "$work_dir/文件格式配置" ]; then
+    rm -r "$work_dir/文件格式配置"
+else
+    mkdir -p "$work_dir/文件格式配置"
+fi
+if [ "$(ls "$work_dir/文件格式配置/")" = "" ]; then
+    if [ -d "$home_dir/ProFile" ]; then
+        cp -r "$home_dir/ProFile/"* "$work_dir/文件格式配置/"
+    else
+        echo "[ $(date) ] No such ProFile! Please Reinstall the module." > "$work_dir/运行日志.log"
+    fi
+fi
+ls "$work_dir/文件格式配置/" | while read f; do
+    name1=$(echo "$f" | cut -f1 -d '.')
+    name2=$(echo "$f" | cut -f2 -d '.')
+    if [ ! "$name2" = "conf" ]; then
+        mv "$work_dir/文件格式配置/$name1.$name2" "$work_dir/文件格式配置/$name1.conf"
+    fi
+done
+}
+prosetting
+######
 if [ ! -f "$work_dir/whitelist.prop" ]; then
     touch "$work_dir/whitelist.prop"
 fi
 if [ ! -f "$work_dir/ClearWhitelist.prop" ]; then
     touch "$work_dir/ClearWhitelist.prop"
-fi
-if [ ! -d "$work_dir/文件格式配置" ]; then
-    if [ -d "$home_dir/ProFile" ]; then
-        cp -r "$home_dir/ProFile" "$work_dir/文件格式配置"
-    else
-        echo "[ $(date) ] No such ProFile! Please Reinstall the module." > "$work_dir/运行日志.log"
-    fi
 fi
 if [ ! -d "$work_dir/清理配置" ]; then
     rm -rf "$work_dir/清理配置"
