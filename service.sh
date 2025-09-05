a7c08610db19574d20d345ca94b14fbc9a4dee84
FlyCome <1453075013@qq.com>
2025-09-05 13:33:42 +0800

    更新 service.sh

--- service.sh
+++ service.sh
@@ -30,47 +30,47 @@ function StartSettings()
 {
     function PropSetting()
     {
-    if [ "$stopcache" = "" ]; then
+    if [ -z "$stopcache" ]; then
         echo "stopcache=0" >> "$work_dir/settings.prop"
         ReSource=1
     fi
-    if [ "$stopinstall" = "" ]; then
+    if [ -z "$stopinstall" ]; then
         echo "stopinstall=0" >> "$work_dir/settings.prop"
         ReSource=1
     fi
-    if [ "$clearall" = "" ]; then
+    if [ -z "$clearall" ]; then
         echo "clearall=0" >> "$work_dir/settings.prop"
         ReSource=1
     fi
-    if [ "$fileall" = "" ]; then
+    if [ -z "$fileall" ]; then
         echo "fileall=0" >> "$work_dir/settings.prop"
         ReSource=1
     fi
-    if [ "$cleardisk" = "" ]; then
+    if [ -z "$cleardisk" ]; then
         echo "cleardisk=1" >> "$work_dir/settings.prop"
         ReSource=1
     fi
-    if [ "$Fileall_Disk" = "" ]; then
+    if [ -z "$Fileall_Disk" ]; then
         echo "Fileall_Disk=1" >> "$work_dir/settings.prop"
         ReSource=1
     fi
-    if [ "$ClearApk_disk" = "" ]; then
+    if [ -z "$ClearApk_disk" ]; then
         echo "ClearApk_disk=1" >> "$work_dir/settings.prop"
         ReSource=1
     fi
-    if [ "$ClearZip_disk" = "" ]; then
+    if [ -z "$ClearZip_disk" ]; then
         echo "ClearZip_disk=1" >> "$work_dir/settings.prop"
         ReSource=1
     fi
-    if [ "$ClearFont_disk" = "" ]; then
+    if [ -z "$ClearFont_disk" ]; then
         echo "ClearFont_disk=0" >> "$work_dir/settings.prop"
         ReSource=1
     fi
-    if [ "$ClearIso_disk" = "" ]; then
+    if [ -z "$ClearIso_disk" ]; then
         echo "ClearIso_disk=0" >> "$work_dir/settings.prop"
         ReSource=1
     fi
-    if [ "$DebugPro" = "" ]; then
+    if [ -z "$DebugPro" ]; then
         echo "DebugPro=0" >> "$work_dir/settings.prop"
         ReSource=1
     fi
@@ -78,11 +78,21 @@ function StartSettings()
     ######
     function prosetting()
     {
-    if [ -f "$work_dir/文件格式配置" ]; then
-        rm -r "$work_dir/文件格式配置"
-    else
-        mkdir -p "$work_dir/文件格式配置"
+    mkdir -p "$work_dir/清理规则"
+    mkdir -p "$work_dir/清理配置"
+    mkdir -p "$work_dir/文件格式配置"
+    mkdir -p "$work_dir/CRON/StopCache"
+    mkdir -p "$work_dir/CRON/ClearCache"
+    mkdir -p "$work_dir/CRON/FileAll"
+    mkdir -p "$work_dir/CRON/ClearDir"
+    ######
+    if [ ! -f "$work_dir/whitelist.prop" ]; then
+        touch "$work_dir/whitelist.prop"
     fi
+    if [ ! -f "$work_dir/ClearWhitelist.prop" ]; then
+        touch "$work_dir/ClearWhitelist.prop"
+    fi
+    ######
     if [ "$(ls "$work_dir/文件格式配置/")" = "" ]; then
         if [ -d "$home_dir/ProFile" ]; then
             cp -r "$home_dir/ProFile/"* "$work_dir/文件格式配置/"
@@ -90,27 +100,13 @@ function StartSettings()
             echo "[ $(date) ] No such ProFile! Please Reinstall the module." > "$work_dir/运行日志.log"
         fi
     fi
-        ls "$work_dir/文件格式配置/" | while read f; do
+    ls "$work_dir/文件格式配置/" | while read f; do
         name1=$(echo "$f" | cut -f1 -d '.')
         name2=$(echo "$f" | cut -f2 -d '.')
         if [ ! "$name2" = "conf" ]; then
             mv "$work_dir/文件格式配置/$name1.$name2" "$work_dir/文件格式配置/$name1.conf"
         fi
     done
-    if [ ! -f "$work_dir/whitelist.prop" ]; then
-        touch "$work_dir/whitelist.prop"
-    fi
-    if [ ! -f "$work_dir/ClearWhitelist.prop" ]; then
-        touch "$work_dir/ClearWhitelist.prop"
-    fi
-    if [ ! -d "$work_dir/清理配置" ]; then
-        rm -rf "$work_dir/清理配置"
-        mkdir -p "$work_dir/清理配置"
-    fi
-    if [ ! -d "$work_dir/清理规则" ]; then
-        rm -rf "$work_dir/清理规则"
-        mkdir -p "$work_dir/清理规则"
-    fi
     }
 ######
 if [ -d "$work_dir" ]; then
@@ -136,16 +132,10 @@ chmod -R 700 "$work_dir"
 StartSettings
 echo "====== ReStart Time $(date) ======" > "$work_dir/运行日志.log"
 ######
-mkdir -p "$work_dir/CRON/StopCache" &
-mkdir -p "$work_dir/CRON/ClearCache" &
-mkdir -p "$work_dir/CRON/FileAll" &
-mkdir -p "$work_dir/CRON/ClearDir" &
-wait
 "$bin_dir/busybox" crond -c "$work_dir/CRON/StopCache" &
 "$bin_dir/busybox" crond -c "$work_dir/CRON/ClearCache" &
 "$bin_dir/busybox" crond -c "$work_dir/CRON/FileAll" &
 "$bin_dir/busybox" crond -c "$work_dir/CRON/ClearDir" &
-wait
 ######
 if [ "$stopcache" = 1 ]; then
     echo -n "* * * * * StopCache Stop" > "$work_dir/CRON/StopCache/root"
@@ -156,7 +146,7 @@ fi
 ######
 if [ "$stopinstall" = 1 ]; then
     chmod 551 /data/app
-elif [ "$stopinstall" = 0 ]; then
+else
     chmod 771 /data/app
 fi
 ######
