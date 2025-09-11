922b54832b75e21b9aa2d29b9dccd1f3011a2541
FlyCome <1453075013@qq.com>
2025-09-12 07:46:53 +0800

    创建 DexOat.sh
    Dexoat脚本（预

---
+++ wipe_cache/DexOat.sh
@@ -0,0 +1,67 @@
+#!/system/bin/sh
+# 此脚本来自ClearBox模块，用于运行DexOat
+if [ ! "$(whoami)" = "root" ]; then
+    echo " » 请授予root权限！"
+    exit 1
+elif ! ClearBox -v >/dev/null; then
+    echo " » 模块加载异常，请排查反馈！"
+    exit 1
+fi
+######
+bin_dir=$(ClearBox -b)
+home_dir=$(ClearBox -h)
+work_dir=$(ClearBox -w)
+source "$work_dir/settings.prop"
+if [ "$DebugPro" = 1 ]; then
+    exec 2>>"$work_dir/运行日志.log"
+else
+    exec 2>>/dev/null
+fi
+######
+function SystemDexOatServer()
+{
+cmd package bg-dexopt-job
+}
+function AllDexOat()
+{
+cmd package compile -m "$option_mode" -f -a
+}
+function AppDexOat()
+{
+cmd package compile -m "$option_mode" -f "$AppPackage"
+}
+# Option in "speed speed-profile everything"
+######
+option_mode="$2"
+AppPackage="$3"
+case "$1" in
+    SYSTEM_DEXOAT)
+        SystemDexOatServer
+        ;;
+    FAST_DEXOAT)
+        if [ -z "$option_mode" ]; then
+            echo " » ERROR：需要一个参数，未传入模式！"
+            exit 1
+        fi
+        AllDexOat
+        ;;
+    APP_DEXOAT)
+        if [ -z "$option_mode" ]; then
+            echo " » ERROR：需要一个参数，未传入模式！"
+            exit 1
+        fi
+        if [ -z "$AppPackage" ]; then
+            echo " » ERROR：需要一个参数，未传入包名！"
+            exit 1
+        fi
+        AppDexOat
+        ;;
+    *)
+        if [ -z "$1" ]; then
+            echo " » ERROR：需要一个参数，未传入选项名称！"
+            exit 1
+        fi
+        ;;
+esac
+######
+exit 0
