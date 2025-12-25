6e98c893dab2da3ba9977901293786e209452745
FLYCOM-E <flycome9798@gmail.com>
2025-12-25 23:50:48 +0000

    Update: 优化Log记录

--- src/BashCore.c
+++ src/BashCore.c
@@ -137,149 +137,149 @@ int main(int argc, char * argv[])
         fileAll2(home_dir, work_dir);
         fastGC(home_dir);
         FreeZer(home_dir);
-        fprintf(log_file_fp, "I [%s] 一键优化清理\n", now_time);
+        fprintf(log_file_fp, "[%s] I 一键优化清理\n", now_time);
     }
     else if (strcasecmp(argv[1], "ClearCache") == 0)
     {
         if (clearCache(home_dir, work_dir) == 0)
         {
-            fprintf(log_file_fp, "I [%s] 清理第三方软件缓存\n", now_time);
+            fprintf(log_file_fp, "[%s] I 清理第三方软件缓存\n", now_time);
         }
         else
         {
-            fprintf(log_file_fp, "E [%s] 清理第三方软件缓存失败\n", now_time);
+            fprintf(log_file_fp, "[%s] E 清理第三方软件缓存失败\n", now_time);
         }
     }
     else if (strcasecmp(argv[1], "Clear_SCache") == 0)
     {
         if (clearSystemCache(home_dir, work_dir) == 0)
         {
-            fprintf(log_file_fp, "I [%s] 清理系统软件缓存\n", now_time);
+            fprintf(log_file_fp, "[%s] I 清理系统软件缓存\n", now_time);
         }
         else
         {
-            fprintf(log_file_fp, "E [%s] 清理系统软件缓存失败\n", now_time);
+            fprintf(log_file_fp, "[%s] E 清理系统软件缓存失败\n", now_time);
         }
     }
     else if (strcasecmp(argv[1], "List_Dir") == 0)
     {
         if (listDir(home_dir, work_dir) == 0)
         {
-            fprintf(log_file_fp, "I [%s] 自定义规则清理\n", now_time);
+            fprintf(log_file_fp, "[%s] I 自定义规则清理\n", now_time);
         }
         else
         {
-            fprintf(log_file_fp, "E [%s] 自定义规则清理失败\n", now_time);
+            fprintf(log_file_fp, "[%s] E 自定义规则清理失败\n", now_time);
         }
     }
     else if (strcasecmp(argv[1], "All_Dir") == 0)
     {
         if (allDir(home_dir, work_dir) == 0)
         {
-            fprintf(log_file_fp, "I [%s] 清理储存目录\n", now_time);
+            fprintf(log_file_fp, "[%s] I 清理储存目录\n", now_time);
         }
         else
         {
-            fprintf(log_file_fp, "E [%s] 清理储存目录失败\n", now_time);
+            fprintf(log_file_fp, "[%s] E 清理储存目录失败\n", now_time);
         }
     }
     else if (strcasecmp(argv[1], "File_Clear") == 0)
     {
         if (fileClear(home_dir, work_dir, argv[2]) == 0)
         {
-            fprintf(log_file_fp, "I [%s] 自定义文件清理\n", now_time);
+            fprintf(log_file_fp, "[%s] I 自定义文件清理\n", now_time);
         }
         else
         {
-            fprintf(log_file_fp, "E [%s] 自定义文件清理失败\n", now_time);
+            fprintf(log_file_fp, "[%s] E 自定义文件清理失败\n", now_time);
         }
     }
     else if (strcasecmp(argv[1], "Clear_App") == 0)
     {
         if (clearApp(home_dir, work_dir, argv[2]) == 0)
         {
-            fprintf(log_file_fp, "I [%s] 自定义软件清理\n", now_time);
+            fprintf(log_file_fp, "[%s] I 自定义软件清理\n", now_time);
         }
         else
         {
-            fprintf(log_file_fp, "E [%s] 自定义软件清理失败\n", now_time);
+            fprintf(log_file_fp, "[%s] E 自定义软件清理失败\n", now_time);
         }
     }
     else if (strcasecmp(argv[1], "File_All") == 0)
     {
         if (fileAll(home_dir, work_dir) == 0)
         {
-            fprintf(log_file_fp, "I [%s] 自定义文件归类\n", now_time);
+            fprintf(log_file_fp, "[%s] I 自定义文件归类\n", now_time);
         }
         else
         {
-            fprintf(log_file_fp, "E [%s] 自定义文件归类失败\n", now_time);
+            fprintf(log_file_fp, "[%s] E 自定义文件归类失败\n", now_time);
         }
     }
     else if (strcasecmp(argv[1], "StopInstall") == 0)
     {
         if (stopInstall(home_dir, work_dir, bin_dir, argv[2]) == 0)
         {
-            fprintf(log_file_fp, "I [%s] %s 阻止安装\n", now_time, argv[2]);
+            fprintf(log_file_fp, "[%s] I %s 阻止安装\n", now_time, argv[2]);
         }
         else
         {
-            fprintf(log_file_fp, "E [%s] %s 阻止安装失败\n", now_time, argv[2]);
+            fprintf(log_file_fp, "[%s] E %s 阻止安装失败\n", now_time, argv[2]);
         }
     }
     else if (strcasecmp(argv[1], "StopStorage") == 0)
     {
         if (stopStorage(home_dir, work_dir, bin_dir, argv[2]) == 0)
         {
-            fprintf(log_file_fp, "I [%s] %s 内部储存固定\n", now_time, argv[2]);
+            fprintf(log_file_fp, "[%s] I %s 内部储存固定\n", now_time, argv[2]);
         }
         else
         {
-            fprintf(log_file_fp, "E [%s] %s 内部储存固定失败\n", now_time, argv[2]);
+            fprintf(log_file_fp, "[%s] E %s 内部储存固定失败\n", now_time, argv[2]);
         }
     }
     else if (strcasecmp(argv[1], "F2fs_GC") == 0)
     {
         if (f2fsGC(home_dir) == 0)
         {
-            fprintf(log_file_fp, "I [%s] F2FS GC\n", now_time);
+            fprintf(log_file_fp, "[%s] I F2FS GC\n", now_time);
         }
         else
         {
-            fprintf(log_file_fp, "E [%s] F2FS GC 失败\n", now_time);
+            fprintf(log_file_fp, "[%s] E F2FS GC 失败\n", now_time);
         }
     }
     else if (strcasecmp(argv[1], "Dexoat_1") == 0)
     {
         if (DexoatSYSTEM_DEXOAT(home_dir) == 0)
         {
-            fprintf(log_file_fp, "I [%s] 触发系统Dexoat\n", now_time);
+            fprintf(log_file_fp, "[%s] I 触发系统Dexoat\n", now_time);
         }
         else
         {
-            fprintf(log_file_fp, "E [%s] 触发系统Dexoat失败\n", now_time);
+            fprintf(log_file_fp, "[%s] E 触发系统Dexoat失败\n", now_time);
         }
     }
     else if (strcasecmp(argv[1], "Dexoat_2") == 0)
     {
         if (DexoatCUST_DEXOAT(home_dir, argv[2]) == 0)
         {
-            fprintf(log_file_fp, "I [%s] 自定义模式Dexoat\n", now_time);
+            fprintf(log_file_fp, "[%s] I 自定义模式Dexoat\n", now_time);
         }
         else
         {
-            fprintf(log_file_fp, "E [%s] 自定义模式Dexoat失败\n", now_time);
+            fprintf(log_file_fp, "[%s] E 自定义模式Dexoat失败\n", now_time);
         }
     }
     else if (strcasecmp(argv[1], "Dexoat_3") == 0)
     {
         if (DexoatCUST_DEXOAT(home_dir, "space") == 0)
         {
-            fprintf(log_file_fp, "I [%s] Dexoat RESET\n", now_time);
+            fprintf(log_file_fp, "[%s] I Dexoat RESET\n", now_time);
         }
         else
         {
-            fprintf(log_file_fp, "E [%s] Dexoat RESET 失败\n", now_time);
+            fprintf(log_file_fp, "[%s] E Dexoat RESET 失败\n", now_time);
         }
     }
     else if (strcasecmp(argv[1], "Freezer") == 0)
