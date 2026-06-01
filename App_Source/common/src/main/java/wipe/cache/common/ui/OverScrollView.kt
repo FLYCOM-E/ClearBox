dcffb18fdee8951da51003fda3ceb41934120536
FLYCOM-E <flycome9798@gmail.com>
2026-06-01 13:42:45 +0800

    Update: App: OverScrollGridView、OverScrollListView、OverScrollView to Kotlin

---
+++ App_Source/common/src/main/java/wipe/cache/common/ui/OverScrollView.kt
@@ -0,0 +1,22 @@
+package wipe.cache.common.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.ScrollView
+
+class OverScrollView : ScrollView {
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyle: Int) : super(
+        context,
+        attrs,
+        defStyle
+    )
+
+    constructor(
+        context: Context?,
+        attrs: AttributeSet?,
+        defStyleAttr: Int,
+        defStyleRes: Int
+    ) : super(context, attrs, defStyleAttr, defStyleRes)
+}
