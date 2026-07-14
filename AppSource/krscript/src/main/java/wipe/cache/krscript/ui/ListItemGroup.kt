c210025c5a80c344556257a5e4919b71a588353b
FLYCOM-E <flycome9798@gmail.com>
2026-07-14 22:10:06 +0800

    Update: App: UI

--- AppSource/krscript/src/main/java/wipe/cache/krscript/ui/ListItemGroup.kt
+++ AppSource/krscript/src/main/java/wipe/cache/krscript/ui/ListItemGroup.kt
@@ -6,6 +6,7 @@ import wipe.cache.krscript.R
 import wipe.cache.krscript.model.GroupNode
 import android.view.View
 import android.util.TypedValue
+import android.widget.LinearLayout
 
 class ListItemGroup(private val ctx: Context,
                      final var isRootGroup: Boolean,
@@ -18,7 +19,7 @@ class ListItemGroup(private val ctx: Context,
     fun addView(item: ListItemView): ListItemGroup {
         val content = layout.findViewById<ViewGroup>(android.R.id.content)
         
-        if (isRootGroup && children.isNotEmpty()) {
+        if (!isRootGroup && children.isNotEmpty()) {
             val space = android.widget.Space(ctx).apply {
                 layoutParams = LinearLayout.LayoutParams(
                     LinearLayout.LayoutParams.MATCH_PARENT,
